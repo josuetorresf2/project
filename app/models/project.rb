62c45bd8896027694eea2b2fd484c4c58248dcae
Joshue Torres <jtorres4@uccs.edu>
2021-11-08 15:34:37 -0700

    update for requirement title and description

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 # The class project validates that title and description are not blank
 # and also makes sure they are not marked for destruction
 class Project < ApplicationRecord
+    #require title and description
     validates_presence_of :title, :description
 end

2a51f5284bb4632b5add18b340c74d5eb00c0fe4
Joshue Torres <jtorres4@uccs.edu>
2021-11-18 16:11:48 -0700

    changes

--- vendor/cache/ruby/2.6.0/gems/nokogiri-1.12.5/ext/nokogiri/tmp/x86_64-apple-darwin20/ports/libxml2/2.9.12/libxml2-2.9.12/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/Users/josuetorresf2/Desktop/Fall 21/3300/project/project/vendor/cache/ruby/2.6.0/gems/nokogiri-1.12.5/ports/x86_64-apple-darwin20/libxml2/2.9.12/lib"
-XML2_LIBS="-lxml2 -lz -L/opt/homebrew/Cellar/xz/5.2.5/lib -llzma -lpthread  -liconv  -lm "
-XML2_INCLUDEDIR="-I/Users/josuetorresf2/Desktop/Fall 21/3300/project/project/vendor/cache/ruby/2.6.0/gems/nokogiri-1.12.5/ports/x86_64-apple-darwin20/libxml2/2.9.12/include/libxml2"
-MODULE_VERSION="xml2-2.9.12"
-

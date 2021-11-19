19b66106317aaff9a5f542aea34ca05f72aa3783
Joshue Torres <jtorres4@uccs.edu>
2021-11-18 20:18:30 -0700

    end

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  it "is a valid email" do
+    expect(email).to be_valid
+  end
+  it "is a valid password" do
+    expect(password).to be_valid
+  end
 end
